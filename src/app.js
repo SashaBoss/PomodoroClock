3f776a41b34a59a2241bbd5ac2e5669710abb3d8
Aleksandr <hiphopkrv@gmail.com>
2017-02-03 16:55:38 +0200

    add project structure

---
+++ src/app.js
@@ -0,0 +1,14 @@
+﻿; (function (angular) {
+
+    var app = angular.module('PomodoroClock', ['ngRoute', 'ngResource']);
+
+    app.config(function ($sceProvider) {
+        $sceProvider.enabled(false);
+    });
+
+    app.controller('PomodoroClockController', [
+        '$scope', '$http', function ($scope, $http) {
+
+        }
+    ]);
+})(window.angular);
